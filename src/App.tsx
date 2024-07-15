4d7eff449c14c73614585c105c14a188092de583
Jack McCallie <mccallie.jack@gmail.com>
2024-07-14 22:59:33 -0500

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BarChart } from 'recharts';
+import MyBarChart from './models/BarChart';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyBarChart />
       </header>
     </div>
   );
